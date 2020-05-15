3b0a837b9ed3e1ff5c29c85860926eadd3e446fc
ronpe <ron.peretz1@gmail.com>
2020-05-15 15:58:39 +0300

    Create 2 html with static css files

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const routers = require('./routes/users')
 
-app.use((req,res,next)=> {
-   console.log(req);
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routers);
+app.use((req, res, next)=> {
+   res.sendFile(path.join(__dirname, 'views', '404.html'))
+})
 
 app.listen(3000);
